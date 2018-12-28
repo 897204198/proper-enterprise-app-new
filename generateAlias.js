0fe4021b55247f323dc30095185f751159a13465
denggy <dengguange@gmail.com>
2018-12-28 17:14:14 +0800

    提交集成工作流方案 (#466)

--- generateAlias.js
+++ generateAlias.js
@@ -4,18 +4,14 @@
 const fs = require('fs');
 
 exports.generateAlias = (webpackConfig) => {
-  fs.readFile('./webpackAlias.txt', 'utf8', (err) => {
-    if (err) {
-      const { alias } = webpackConfig.resolve
-      let tempStr = ''
-      for (const key in alias) {
-        tempStr += `${key}: ${alias[key]}\r\n`
-      }
-      fs.writeFile('./webpackAlias.txt', tempStr, (error) => {
-        if (error) {
-          console.log(error);
-        }
-      })
+  const { alias } = webpackConfig.resolve
+  let tempStr = ''
+  for (const key in alias) {
+    tempStr += `${key}: ${alias[key]}\r\n`
+  }
+  fs.writeFile('./webpackAlias.txt', tempStr, (error) => {
+    if (error) {
+      console.log(error);
     }
   })
 }
