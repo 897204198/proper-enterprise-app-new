0d8258c24863f833461554d97153f69f2afb3aa5
wukongnalizou <37323494+wukongnalizou@users.noreply.github.com>
2019-07-25 11:55:32 +0800

    自动化脚本修改 (#730)

--- tests/selenium.js
+++ tests/selenium.js
@@ -1,14 +1,24 @@
 const fs = require('fs');
+const SCP = require('simple-scp');
 const path = require('path');
 const { execSync } = require('child_process');
 
 // 解析需要遍历的文件夹，我这以E盘根目录为例
 const filePath = path.resolve('./tests/modules');
 const seleniumRunnerPath = path.resolve('./node_modules/proper-selenium-side-runner/dist/index.js');
+// const resultPath = path.resolve('./result/html-report');
 
-console.log(filePath)
+const serverConfig = {
+  up: {
+    host: '59.110.140.112',
+    port: 22022,
+    username: 'root',
+    password: 'ds@Proper20!^'
+  }
+};
+
 const fileResult = []
-
+// const errPath = []
 readDirSync(filePath)
 
 execute()
@@ -22,10 +32,13 @@ function execute() {
       execSync(`node ${seleniumRunnerPath} ${fileResult[i]} --output-directory=result --output-html=true --output-format=junit`)
     } catch (ex) {
       errs.push(ex);
+      // errPath.push(fileResult[i]);
       console.log('此用例失败')
     }
     if (i === fileResult.length - 1 && errs.length > 0) {
-      throw errs
+      const deploy = new SCP(serverConfig.up);
+      deploy.dest('./result/html-report/', '/root/');
+      // throw errs
     }
   }
 }
